89a11d7fc2c2d8480e481cbaecd76e59d9abc286
Rohit Mahajan <rm90087@gmail.com>
2025-03-04 02:18:08 +0530

    online user's issue fixed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
 const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:3000":"/" 
+
 export const useAuthStore=create((set,get)=>({
     authUser:null,
     isSigningUp:false, 
@@ -83,7 +84,7 @@ export const useAuthStore=create((set,get)=>({
     connectSocket:async()=>{
         const {authUser}=get();
         if(!authUser || get().socket?.connected)return;
-        const socket=io("http://localhost:3000",{
+        const socket=io(BASE_URL,{
             query:{
                 userId:authUser._id
             }
@@ -93,9 +94,7 @@ export const useAuthStore=create((set,get)=>({
         set({socket:socket})
 
         socket.on("getOnlineUsers",(users)=>{
-            set({onlineUsers:users});
-            console.log("online users",get().onlineUsers);
-            
+            set({onlineUsers:users});            
         })
         
     },
