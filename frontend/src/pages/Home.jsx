f38b821729b435beb6cf6a421d37df5430a819b9
Rohit Mahajan <rm90087@gmail.com>
2025-03-04 01:11:22 +0530

    bug fixed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useChatStore } from '../store/useChatStore.js'
-import ChatContainer from '../components/chatContainer';
+import ChatContainer from '../components/ChatContainer';
 import NoChatSelected from '../components/NoChatSelected';
 import Sidebar from '../components/sidebar.jsx';
 const Home = () => {
